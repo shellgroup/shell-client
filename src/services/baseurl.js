194ae3138ee42584e36bc8f65171cede47b7b4ad
shuaikangzhang <303316861@qq.com>
2019-01-28 16:25:40 +0800

    修改baseurl

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,11 +1,13 @@
-let flag = 'dev'; // dev开发版  pro生产版 local本地
+let flag = 'test'; // dev开发版  pro生产版 local本地
 module.exports = {
   baseURL:
-    flag === 'pro'
+    flag === 'test'
       ? '/api'
       : flag === 'dev'
       ? 'http://192.168.199.109:9001/master-admin'
       : flag === 'local'
       ? 'http://localhost:9001/master-admin'
+      : flag === 'pro'
+      ? '/api'
       : null,
 };
