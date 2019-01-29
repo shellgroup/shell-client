583ff71ed07878155e30e297061f5af918c95882
ZhangWenBin <wenbin3373@qq.com>
2019-01-29 16:54:43 +0800

    文件上传

---
+++ src/pages/SystemManager/DruidSql.js
@@ -0,0 +1,23 @@
+import React, { PureComponent, Fragment } from 'react';
+import { baseURL } from '../../services/baseurl';
+import { Card } from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import styles from './DruidSql.less';
+class DruidSql extends PureComponent {
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <Card bordered={false}>
+          <div className={styles.tableList}>
+            <div className={styles.tableListForm} />
+            <div className={styles.box}>
+              <iframe src={`${baseURL}/druid/sql.html`} className={styles.iheight} />
+            </div>
+          </div>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default DruidSql;
